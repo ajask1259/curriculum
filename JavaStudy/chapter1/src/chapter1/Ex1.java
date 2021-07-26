c00446fd0a83c677bd043ee042e025f72b462961
ajask1259 <79787254+ajask1259@users.noreply.github.com>
2021-07-27 00:51:41 +0900

    課題2_1
    課題完成

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+import java.util.ArrayList;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		java.util.List<String> list = new ArrayList<String>();{
+			list.add("Hello java");
+		}
+
+	}
+
+}
