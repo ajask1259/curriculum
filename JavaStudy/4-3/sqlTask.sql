a906fb7aa2f85fd6eb90b05489de00ee15153251
ajask1259 <79787254+ajask1259@users.noreply.github.com>
2021-09-17 22:32:30 +0900

    課題4-3
    課題修正

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -12,12 +12,12 @@ SELECT * FROM store_table ORDER BY store_nameAbc ASC
  
 -- 3. 在庫テーブルに店舗テーブル、商品テーブルを「内部結合」し、店舗名・商品名・在庫数を全て取得しなさい。
 -- [回答]
-SELECT goods_table.goods_name,store_table.store_name,stock_table.quantity 
+SELECT goods_name,store_name,quantity 
 FROM stock_table 
 JOIN goods_table 
-ON goods_table.goods_code = stock_table.goods_code 
+ON stock_table.goods_code = goods_table.goods_code 
 JOIN store_table 
-ON store_table.store_code = stock_table.store_code
+ON stock_table.store_code = store_table.store_code
  
  
 -- 4. 商品テーブルから全商品の単価の平均値を抽出しなさい。
@@ -27,8 +27,8 @@ SELECT AVG(price) FROM goods_table
  
 -- 5. 店舗コード='EA01'の在庫数の平均値より大きい在庫数を持つ店舗コードを抽出しなさい。
 -- [回答]
-SELECT store_code, AVG(quantity) FROM stock_table GROUP BY store_code
-HAVING AVG(quantity) > 16
+SELECT store_code FROM stock_table
+WHERE quantity > (SELECT AVG(quantity) FROM stock_table WHERE store_code='EA01')
  
 -- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
