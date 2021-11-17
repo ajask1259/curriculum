f00772c598f2e606f900d7cef0ced0b043bfa5d8
ajask1259 <79787254+ajask1259@users.noreply.github.com>
2021-11-17 23:31:10 +0900

    課題Check5

--- JavaStudy/Check4/src/main/java/controller/EmployeeController.java
+++ JavaStudy/Check4/src/main/java/controller/EmployeeController.java
@@ -33,8 +33,7 @@ public class EmployeeController extends HttpServlet {
   // 問② EmployeeServiceクラスをインスタンス化する。
  EmployeeService employeeService = new EmployeeService();
   // 問③ EmployeeBeanに、EmployeeServiceよりsearch関数を呼び出し、返り値を格納する。
- EmployeeBean employeeBean = new EmployeeBean();
- employeeBean = employeeService.search(id,password); 
+ EmployeeBean employeeBean = employeeService.search(id,password); 
   // 問④ nullの部分に適切な引数をセットする。
  request.setAttribute("EmployeeBean",employeeBean);
  
